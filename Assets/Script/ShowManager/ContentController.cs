9e7d4d923291c84d1456e3ac69af7b25b4160415
OnlyShoky <leozul@hotmail.es>
2019-04-29 16:22:30 +0200

    Save Load Functionalities

--- Assets/Script/ShowManager/ContentController.cs
+++ Assets/Script/ShowManager/ContentController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 
 
@@ -10,25 +11,20 @@ public class ContentController : MonoBehaviour
 {
 
     public GameObject controllerObject ;
-
-
-
     public List<GameObject> gameObjectList;
-    
     // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
     public GameObject myPrefab;
     public InputField commandText;
-    
+    private List<string> contentDataList;
 
 
-    public void AddObjectToList(string name){
 
+    //Add a command to the list , the text in the input field will be the command , and the number is the last number plus one
+    public void AddObjectToList(){
 
         gameObjectList.Add(Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity,gameObject.transform));
         int index = gameObjectList.Count -1;
-        
-        print(index) ;
-    
+            
         Text[] components = gameObjectList[index].GetComponentsInChildren<Text>();
         gameObjectList[index].name = "(" + gameObjectList.Count.ToString() + ")";
         components[0].text = gameObjectList.Count.ToString();
@@ -38,19 +34,66 @@ public class ContentController : MonoBehaviour
 
     }
 
+    public void AddObjectToList(string name){
+
+        gameObjectList.Add(Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity,gameObject.transform));
+        int index = gameObjectList.Count -1;
+            
+        Text[] components = gameObjectList[index].GetComponentsInChildren<Text>();
+        gameObjectList[index].name = "(" + gameObjectList.Count.ToString() + ")";
+        components[0].text = gameObjectList.Count.ToString();
+        components[1].text = name;
+
+
+    }
+
+    //Remove the last object of the list
     public void RemoveLastObject(){
-        print("Nombre de gameobject antes de elminacion : " + gameObjectList.Count);
         Destroy(gameObjectList[gameObjectList.Count-1]);
         gameObjectList.Remove(gameObjectList[gameObjectList.Count-1]);
-        print("Nombre de gameobject despues de elminacion : " + gameObjectList.Count);
 
     }
 
+    //Save list to json
     public void SaveList(){
-        string json = JsonUtility.ToJson(gameObjectList);
-        Debug.Log(json);
+        
+        
+        CommandList commandList = new CommandList();
+        List<string> contentDataList = new List<string>();
+        
+        foreach (var item in gameObjectList)
+        {
+            Text[] components = item.GetComponentsInChildren<Text>();
+            Controller controller = controllerObject.GetComponentInChildren<Controller>();
+            contentDataList.Add(components[1].text);
+        }
+
+        commandList.contentDataList = contentDataList ;
+        string json = JsonUtility.ToJson(commandList);
+        File.WriteAllText(Application.dataPath+"/DATA/saveFile.json",json);
+        print(Application.dataPath);
+        print(json);
+        
+
+ 
     }
 
+    public void LoadList(){
+
+        string json = File.ReadAllText(Application.dataPath+"/DATA/saveFile.json");
+        CommandList loadedGameObjectList = JsonUtility.FromJson<CommandList>(json);
+        int nbElements = gameObjectList.Count;
+
+        for(int i = 0; i<nbElements ; i++){
+            RemoveLastObject();
+        }
+        foreach(var item in loadedGameObjectList.contentDataList){
+            AddObjectToList(item);
+        }
+
+    }
+
+    //Excute all the commands from the list
     public void PlayAllCommands(){
         foreach (var item in gameObjectList)
         {
@@ -59,11 +102,17 @@ public class ContentController : MonoBehaviour
             print(controller.name + ": "+ components[1].text);   
             controller.WriteToArduino(components[1].text);
 
-              
-
         }
 
     }
+
+    [System.Serializable]
+    public class CommandList{
+        public List<string> contentDataList;
+
+    }
+
+
 
 
  
